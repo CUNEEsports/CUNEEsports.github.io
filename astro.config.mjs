43cfbca93e9796ce5f3c3b677d53ae777e306dae
jwood2020 <99372318+jwood2020@users.noreply.github.com>
2023-11-13 13:53:13 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://CUNEEsports.github.io',
+    site: 'https://cuneesports.github.io',
 });
