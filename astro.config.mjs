7e4d9869eba1812017ac99d0ca24b0aa95ddb0ae
jwood2020 <99372318+jwood2020@users.noreply.github.com>
2023-11-13 14:16:33 -0600

    Attempt #5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-<<<<<<< HEAD
-    site: 'https://cuneesports.github.io',
-});
-=======
     site: 'https://CUNEEsports.github.io',
   })
->>>>>>> master
